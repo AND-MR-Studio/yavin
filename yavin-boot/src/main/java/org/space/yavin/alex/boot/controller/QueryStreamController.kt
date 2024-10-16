4c7d448491f126aa0352fdf401e94be3349e2346
alex <hfyy0726@outlook.com>
2024-10-16 21:49:21 +0800

    集成LangChain4j实现AI助手功能
    - 在yavin-boot模块中添加AI助手相关代码，包括Assistant和AssistantController类
    - 在yavin-service模块中添加LangChain4j相关依赖
    - 更新yavin-boot的pom.xml，添加LangChain4j Spring Boot Starter依赖
    - 在应用配置文件中配置LangChain4j相关属性

---
+++ yavin-boot/src/main/java/org/space/yavin/alex/boot/controller/QueryStreamController.kt
@@ -0,0 +1,31 @@
+package org.space.yavin.alex.boot.controller
+
+import dev.ai4j.openai4j.chat.Content
+import org.springframework.http.ResponseEntity
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import reactor.core.publisher.Flux
+
+/**
+ * @author yyHuangfu
+ * @create 2024/10/6
+ */
+class QueryStreamController {
+
+    @PostMapping("/stream-query")
+    fun internalStreamPost(
+        serverHttpRequest: ServerHttpRequest,
+        @RequestBody dialogRequest: DialogRequest
+    ): ResponseEntity<Flux<StreamDialogResult>> {
+        val res = StreamDialogResult("init");
+        return ResponseEntity.ok(Flux.just(res))
+    }
+}
+
+class DialogRequest{
+    var query: String? = null
+}
+
+class StreamDialogResult constructor(var content: String){
+}
---
+++ yavin-boot/src/main/java/org/space/yavin/alex/boot/controller/YavinBootApp.java
@@ -0,0 +1,19 @@
+package org.space.yavin.alex.boot.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yyHuangfu
+ * @create 2024/9/13
+ */
+
+@SpringBootApplication
+public class YavinBootApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(YavinBootApp.class, args);
+
+    }
+}
